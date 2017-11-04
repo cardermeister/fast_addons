24baeccaba6f485f5734a121467dbf82bfeccb11
_A <smeash1997@gmail.com>
2017-11-04 11:25:07 +0000

    gsub("\n"," ")

--- lua/main/server/gitify.lua
+++ lua/main/server/gitify.lua
@@ -3,8 +3,10 @@ function string.gitify(json)
 	json = util.JSONToTable(json).payload.commits
 	
 	for i,k in pairs(json) do
+	    
+		local message = k.message:gsub("\n"," ")
 		
-		ChatAddText(Color(117, 113, 94),"[fast_addons:master]",color_white," Commit from ",Color(255,140,113),k.author.name,color_white,"\n\t♢ ",k.message)
+		ChatAddText(Color(117, 113, 94),"[fast_addons:master]",color_white," Commit from ",Color(255,140,113),k.author.name,color_white,"\n\t♢ ",message)
 		
 		if (#k.added>0) then local buff = {color_white,"\t┕ ",Color(0,255,0),"Added: ",color_white} for j,v in pairs(k.added) do table.insert(buff,v) end ChatAddText(unpack(buff))  end
 		if (#k.modified>0) then buff = {color_white,"\t┕ ",Color(255,191,0),"Modified: ",color_white} for j,v in pairs(k.modified) do table.insert(buff,v) end ChatAddText(unpack(buff))  end
