db9df72dd676b52348a43a7fe2125020552598e2
_A <smeash1997@gmail.com>
2017-11-10 19:01:22 +0000

    u wot mate??

--- lua/main/server/discord.lua
+++ lua/main/server/discord.lua
@@ -1,28 +1,34 @@
-local webhook = "https://discordapp.com/api/webhooks/378116447605620736/z8UAE5XXQMAlpLCbvM8gd25jh17Jopg6rVGNkvvfgvlbgc65J5cgJ69U--SRdkg5FCD8"
+hook.Add("PostGamemodeLoaded","discord.lua",function()
 
-discord = discord or {}
+    local webhook = "https://discordapp.com/api/webhooks/378116447605620736/z8UAE5XXQMAlpLCbvM8gd25jh17Jopg6rVGNkvvfgvlbgc65J5cgJ69U--SRdkg5FCD8"
+    
+    discord = discord or {}
+    
+    function discord.print(...)
+    	local str = ""
+    	for i,k in pairs({...}) do
+    		str=str + tostring(k) +" "
+    	end
+    	http.Post(webhook,{content = str})
+    	return print(...)
+    end
+    
+    function discord.PrintTable(...)
+    	
+    	http.Post(webhook,{content = table.ToString(...,nil,true)})
+    	return PrintTable(...)	
+    end
+    
+    
+    concommand.Add("discord-lua-run",function(ply)
+    	
+    	if ( IsValid(ply) ) then return end
+    	
+    	easylua.RunLua(nil,file.Read("discord-lua.txt","DATA"))
+    	file.Delete("discord-lua.txt")
+    
+    end)
 
-function discord.print(...)
-	local str = ""
-	for i,k in pairs({...}) do
-		str=str + tostring(k) +" "
-	end
-	http.Post(webhook,{content = str})
-	return print(...)
-end
-
-function discord.PrintTable(...)
-	
-	http.Post(webhook,{content = table.ToString(...,nil,true)})
-	return PrintTable(...)	
-end
-
-
-concommand.Add("discord-lua-run",function(ply)
-	
-	if ( IsValid(ply) ) then return end
-	
-	easylua.RunLua(nil,file.Read("discord-lua.txt","DATA"))
-	file.Delete("discord-lua.txt")
+    hook.Remove("PostGamemodeLoaded","discord.lua")
 
 end)
