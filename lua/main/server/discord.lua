2c884163940e4f3807be05e58595418be61f0aa3
_A <smeash1997@gmail.com>
2017-11-13 12:57:57 +0000

    Update discord.lua

--- lua/main/server/discord.lua
+++ lua/main/server/discord.lua
@@ -1,41 +1,35 @@
-hook.Add("PostGamemodeLoaded","discord.lua",function()
+local webhook = "https://discordapp.com/api/webhooks/378116447605620736/z8UAE5XXQMAlpLCbvM8gd25jh17Jopg6rVGNkvvfgvlbgc65J5cgJ69U--SRdkg5FCD8"
 
-    local webhook = "https://discordapp.com/api/webhooks/378116447605620736/z8UAE5XXQMAlpLCbvM8gd25jh17Jopg6rVGNkvvfgvlbgc65J5cgJ69U--SRdkg5FCD8"
-    
-    discord = discord or {}
-    
-    function discord.print(...)
-    	local str = ""
-    	for i,k in pairs({...}) do
-    		str=str + tostring(k) +" "
-    	end
-    	http.Post(webhook,{content = str})
-    	return print(...)
-    end
-    
-    function discord.PrintTable(...)
-    	
-    	http.Post(webhook,{content = table.ToString(...,nil,true)})
-    	return PrintTable(...)	
-    end
-    
-    
-    concommand.Add("discord-lua-run",function(ply,cmd,arg,line)
-    	
-    	if ( IsValid(ply) ) then return end
-		
-		local luacode = file.Read("discord-lua.txt","DATA")
-		
-    	MsgC"[discord] "print("running lua by",line)
-    	if easylua.RunLua(nil,luacode).error then
-			RunString(luacode)
-		end
+discord = discord or {}
 
-    
-    end)
+function discord.print(...)
+	local str = ""
+	for i,k in pairs({...}) do
+		str=str + tostring(k) +" "
+	end
+	http.Post(webhook,{content = str})
+	return print(...)
+end
 
-    discord.print("[INIT] Server successfully started")
+function discord.PrintTable(...)
+	
+	http.Post(webhook,{content = table.ToString(...,nil,true)})
+	return PrintTable(...)	
+end
 
-    hook.Remove("PostGamemodeLoaded","discord.lua")
+
+concommand.Add("discord-lua-run",function(ply,cmd,arg,line)
+	
+	if ( IsValid(ply) ) then return end
+	
+	local luacode = file.Read("discord-lua.txt","DATA")
+	
+	Msg"[discord] "print("running lua by",line or "god")
+	if easylua.RunLua(nil,luacode).error then
+		RunString(luacode)
+	end
+
 
 end)
+
+discord.print("[INIT] discord.lua successfully loaded")
