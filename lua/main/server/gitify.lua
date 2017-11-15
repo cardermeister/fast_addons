867d65c25072b331d7696820121f297c02761966
Card <smeash1997@gmail.com>
2017-11-16 00:41:11 +0700

    git notify

--- lua/main/server/gitify.lua
+++ lua/main/server/gitify.lua
@@ -1,9 +1,9 @@
-local function func(k,commit_add)
+local function func(repo_name,k,commit_add)
 	
 	local message = k.message:gsub("\n"," ")
 	
 	
-	ChatAddText(Color(117, 113, 94),"[fast_addons:master]",color_white," Commit from ",Color(255,140,113),k.author.name,color_white,"\n\t♢ ",message)
+	ChatAddText(Color(117, 113, 94),"["+repo_name+":master]",color_white," Commit from ",Color(255,140,113),k.author.name,color_white,"\n\t♢ ",message)
 	
 	if (#k.added>0) then local buff = {color_white,"\t┕ ",Color(0,255,0),"Added: ",color_white} for j,v in pairs(k.added) do local zap = "" if(j>1)then zap=", " end table.insert(buff,zap..v) end ChatAddText(unpack(buff))  end
 	if (#k.modified>0) then buff = {color_white,"\t┕ ",Color(255,191,0),"Modified: ",color_white} for j,v in pairs(k.modified) do local zap = "" if(j>1)then zap=", " end table.insert(buff,zap..v) end ChatAddText(unpack(buff))  end
@@ -20,6 +20,7 @@ end
 local function gitify(json)
 
 	json = util.JSONToTable(json).payload
+	local repo_name = json.repository.name
 	local projectid = json.project_id
 	json = json.commits
 	
@@ -29,9 +30,9 @@ local function gitify(json)
 		
 		http.Fetch(("https://gitlab.com/api/v4/projects/%s/repository/commits/%s"):format(projectid,sha),
 		function(a)
-			func(k,util.JSONToTable(a))
+			func(repo_name,k,util.JSONToTable(a))
 		end,function(s)
-		func(k)
+		func(repo_name,k)
 		end,{["Private-Token"]="9BtwmHamzRwiLdsijELR"})
 		
 	end
