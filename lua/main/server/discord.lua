f13bc31d4553e50809f4c9f92900207f83caed0a
Card <smeash1997@gmail.com>
2017-11-20 00:37:51 +0700

    Function Raw ``` fix fix

--- lua/main/server/discord.lua
+++ lua/main/server/discord.lua
@@ -55,7 +55,7 @@ hook.Add("PlayerSay","discord_relay_chat", discord.relay_func)
 local env = {
 	print = discord.print,
 	PrintTable = discord.PrintTable,
-	GetFunctionRaw = function(...) return GetFunctionRaw(...):gsub("```","` ` `") end
+	GetFunctionRaw = function(...) return GetFunctionRaw(...):gsub("```","` ` `")[1] end
 }
 local meta = {}
 meta.__index = _G
