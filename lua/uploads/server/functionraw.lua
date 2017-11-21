405f206398a43aff3f4ea3451c36725a1dcd28fc
CornerPin <cornerpinart@gmail.com>
2017-11-21 21:39:55 +0300

    Restruct the for loop, fix syntax, add comments

--- lua/uploads/server/functionraw.lua
+++ lua/uploads/server/functionraw.lua
@@ -1,34 +1,36 @@
-function GetFunctionRaw( func )
-	assert( isfunction( func ), "bad argument #1 to 'GetFunctionRaw' (function expected, got " .. type( func ) .. ")" )
+function GetFunctionRaw(func)
+	assert(isfunction(func), "bad argument #1 to 'GetFunctionRaw' (function expected, got " .. type(func) .. ")")
 	
 	local ret = {}
 	
-	local info = debug.getinfo( func, "S" )
-	if info.what ~= "Lua" then return "[C]" end
+	local info = debug.getinfo(func, "S")
+	if info.what ~= "Lua" then return end
 	
 	local src = info.short_src
 
-	local fileRaw = file.Read( src, "GAME" ) or file.Read( src, "LUA" )
+	local fileRaw = file.Read(src, "GAME") or file.Read(src, "LUA")
 	if fileRaw == nil then return end
 	
 	
-	local lines = string.Explode( "\n", fileRaw )
-	
+	local lines = string.Explode("\n", fileRaw)
 	local minIndents = math.huge
 	local needToTrimIndents = true
 	
 	for i = info.linedefined, info.lastlinedefined do
 		local line = lines[i]
 		
-		if line:Trim(" ") ~= "" then
+		-- if [we need to trim] and [the line isn't empty]
+		if needToTrimIndents and line:Trim(" ") ~= "" then
+			-- Match the first tabs sequence
 			local tabs = line:match("^\t+")
 			
-			if needToTrimIndents then
-				if tabs then
-					minIndents = math.min(#tabs, minIndents)
-				else
-					needToTrimIndents = false
-				end
+			-- If we have indentation on this line
+			if tabs then
+				-- Then choose between shortest tab sequence and save its length
+				minIndents = math.min(#tabs, minIndents)
+			else
+				-- Otherwase abort indentation fix
+				needToTrimIndents = false
 			end
 		end
 		
@@ -36,16 +38,19 @@ function GetFunctionRaw( func )
 	end
 	
 	
+	-- If indentation fix wasn't aborted
 	if needToTrimIndents then
+		-- Tab sequence we need to remove
 		local removePattern = "^" .. string.rep("\t", minIndents)
 		
 		for i, line in ipairs(ret) do
 			ret[i] = line:gsub(removePattern, "")
 		end
 	end
+
 	
-	
+	-- Insert the first/last line and the source file path
 	table.insert(ret, 1, string.format("-- [%i:%i] %s", info.linedefined, info.lastlinedefined, src))
 	
-	return table.concat( ret, "\n" )
+	return table.concat(ret, "\n")
 end
