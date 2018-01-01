6115e182c97cd139472d0c02ec964973f67573c6
CornerPin <cornerpinart@gmail.com>
2018-01-01 17:24:17 +0300

    Fix iin concommand

--- lua/main/modules/chatcommands.lua
+++ lua/main/modules/chatcommands.lua
@@ -5,7 +5,7 @@ iin = iin or {}
 iin.Prefix = "!"
 local EchoCmds=CreateConVar( Tag.."_echo_luacmds", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE,FCVAR_NOTIFY} )
 
-function iin.AddCommand(cmd,callback,group,hidechat)
+function iin.AddCommand(cmd,callback,IsUserGroup(string groupname),hidechat)
 	iin.cmds = iin.cmds or {}
 	iin.cmds[cmd] = {callback = callback, group = group or "players", cmd = cmd, hidechat = hidechat or false}
 end
@@ -24,16 +24,16 @@ cmd = iin.cmds[cmd]
 end
 
 
-function iin.ChatCommands(ply,txt)
-	if txt:sub(1, 1):find(iin.Prefix) then
-		local cmd = txt:match(iin.Prefix.."(.-) ") or txt:match(iin.Prefix.."(.+)") or ""
-		local line = txt:match(iin.Prefix..".- (.+)")
+function iin.ChatCommands(ply, txt)
+	if string.sub(txt, 1, 1) == iin.Prefix then
+		local cmd = txt:match("^" .. iin.Prefix .. "(%S*)") or ""
+		local line = txt:sub(#cmd + 2)
 		
 		cmd = cmd:lower()
 		local iincmd = iin.cmds[cmd]
 		if iincmd then
-			if ply:CheckGroupPower(iin.cmds[cmd].group) then
-				iin.CallCommand(ply,cmd,line,line and iin.ParseArgs(line) or {})
+			if ply:CheckGroupPower(iincmd.group) then
+				iin.CallCommand(ply, cmd, line, line and iin.ParseArgs(line) or {})
 				if iincmd.hidechat then return "" end
 			end
 		end
@@ -42,21 +42,19 @@ end
 hook.Add("PlayerSay", "chatcmd", iin.ChatCommands)
  
 concommand.Add(Tag, function(ply, _, _, argline)
-	--[[txt = table.concat(txt," ")
-	local cmd = txt:match("(.-) ") or txt:match(iin.Prefix.."(.+)") or ""
-	local line = txt:match(".- (.+)")
+	local cmd = argline:match("^%s*(%S*)") or ""
+	local line = argline:sub(#cmd + 2)
 	
 	cmd = cmd:lower()
-	if iin.cmds[cmd] then
-		if ply:CheckGroupPower(iin.cmds[cmd].group) then
-			iin.CallCommand(ply,cmd,line,line and iin.ParseArgs(line) or {})
-			return true
+	local iincmd = iin.cmds[cmd]
+	if iincmd then
+		if ply:CheckGroupPower(iincmd.group) then
+			iin.CallCommand(ply, cmd, line, line and iin.ParseArgs(line) or {})
 		end
-	end]]
-	iin.ChatCommands(ply, argline)
+	end
 end)
 
 iin.AddCommand("say", function(player, line)
 	if not line or line=="" then return end
 	timer.Simple(0,function()luadev.RunOnClients("Say(" .. line .. ")") end)
-end, "devs") 
+end, "devs", true) 
