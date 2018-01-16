38999bc0518fb28d37ba88feb3286fcd2457d478
CornerPin <cornerpinart@gmail.com>
2018-01-16 21:29:45 +0300

    Fix setname

--- lua/uploads/setname.lua
+++ lua/uploads/setname.lua
@@ -1,75 +1,98 @@
-local meta = FindMetaTable( "Player" )
+local PLAYER = FindMetaTable("Player")
+
 
 if SERVER then
-	
-	local function AllowedToSetName(ply,name)
+	local function CanChangeName(ply, name)
+		local oldname = ply:Nick()
+		local newname = (name == "" or name == nil) and ply:DefaultName() or name
+		if oldname == newname then return false end
+		
 		if not ply:IsAdmin() then
 			if name then
 				if (utf8.len(name) or #name) > 32 then
-					ply:ChatPrint( "Слишком длинный ник." )
-					return
+					ply:ChatPrint("Слишком длинный ник.")
+					return false
 				end
 			end
 			
-			local cooldown = ply.namecldwn
-			if not cooldown then ply.namecldwn = 0 end
+			local cooldown = ply.nameCooldown
+			if not cooldown then cooldown = 0 end
 			
 			local curtime = CurTime()
-			if curtime < ply.namecldwn then
-				ply:ChatPrint( Format( "Подождите ещё %i сек., прежде чем изменить ник.", ply.namecldwn - curtime ) )
-				return
+			if curtime < cooldown then
+				ply:ChatPrint(Format(
+					"Подождите ещё %i сек., прежде чем изменить ник.",
+					ply.nameCooldown - curtime
+				))
+				
+				return false
 			end
 			
-			ply.namecldwn = CurTime() + 30
+			ply.nameCooldown = curtime + 30
 		end
 		
 		return true
 	end
 	
-	meta.SetName = function(ply,name,donotverb)
-		if not name then return end
-		name = name:gsub( "\n", "" )
+	function PLAYER:SetName(name, verbose)
+		local oldname = self:Nick()
+		local newname
 		
-		local FName = ply:GetNWString( "FName" )
+		if not name then
+			self:SetNWString("FName", "")
+			newname = self:DefaultName()
+		else
+			name = name:gsub("\n", "")
+			self:SetNWString("FName", name)
+			
+			newname = name == "" and self:DefaultName() or name
+		end
 		
-		ply:SetNWString( "FName", name )
-		
-		if donotverb then return end
-		
-		all:ChatPrint( Format(
-			"Player %s changed name to %s",
-			FName ~= "" and FName or ply:DefaultName(),
-			name
-		) )
+		if verbose then
+			all:ChatPrint(Format(
+				"Player %s changed name to %s",
+				oldname,
+				newname
+			))
+		end
 	end
 	
-	iin.AddCommand( "setname", function(ply,name)
-		if AllowedToSetName(ply,name) then ply:SetName(name) end
-	end, "players", true )
+	iin.AddCommand("setname", function(ply, name)
+		if CanChangeName(ply, name) then
+			ply:SetName(name, true)
+		end
+	end, "players", true)
 	
-	iin.AddCommand( "defname", function(ply)
-		if AllowedToSetName(ply) then ply:SetName(ply:DefaultName()) end
-	end, "players", true )
-	
+	iin.AddCommand("defname", function(ply)
+		if CanChangeName(ply, name) then
+			ply:SetName("", true)
+		end
+	end, "players", true)
 end
 
-if not meta.DefaultName then
-	meta.DefaultName = meta.Nick	
+
+if not PLAYER.DefaultName then
+	PLAYER.DefaultName = PLAYER.Nick	
 end
 
-meta.Nick = function( ply )
-	local FName = ply:GetNWString( "FName" )
+function PLAYER:Nick()
+	local name = self:GetNWString("FName")
 
-	if FName != "" then
-		return FName
+	if name ~= "" then
+		return name
 	else
-		return ply:DefaultName()
+		return self:DefaultName()
 	end
- end
+end
 
-meta.GetName = meta.Nick
-meta.Name = meta.Nick
 
---[[meta.__tostring = function( self )
-	return string.format( "Player [%d][%s]", self:EntIndex(), self:Nick() )
-end]]
+PLAYER.GetName = PLAYER.Nick
+PLAYER.Name = PLAYER.Nick
+
+function PLAYER:__tostring()
+	if IsValid(self) then
+		return string.format("Player [%d][%s]", self:EntIndex(), self:Nick())
+	end
+	
+	return "Player [NULL]"
+end
