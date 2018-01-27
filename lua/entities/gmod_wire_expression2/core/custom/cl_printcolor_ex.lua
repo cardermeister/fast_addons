0cff6ca6ac3b8316ac46135699819dd46a5d0540
CornerPin <cornerpinart@gmail.com>
2018-01-27 14:31:40 +0300

    Too abusable

--- lua/entities/gmod_wire_expression2/core/custom/cl_printcolor_ex.lua
+++
@@ -1,4 +0,0 @@
-E2Helper.Descriptions["printColor(e:...)"] = "Like printColor(...), but prints to the specified player"
-E2Helper.Descriptions["printColor(e:r)"] = "Like printColor(r), but prints to the specified player"
-E2Helper.Descriptions["printColor(r:...)"] = "Like printColor(...), but prints to the specified group of players"
-E2Helper.Descriptions["printColor(r:r)"] = "Like printColor(r), but prints to the specified group of players"
