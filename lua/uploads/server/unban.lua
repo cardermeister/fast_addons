2585f5366b1933c9db59a54b2f31fc2e5c9913dc
CornerPin <cornerpinart@gmail.com>
2018-02-20 19:48:17 +0300

    Micro optimization: pairs -> ipairs Change code formatting, no more whitespaces before and after brackets New utility function "BanReason(steamid32/64)" - returns ban reason as string or no value, if the player isn't found

--- lua/uploads/server/unban.lua
+++ lua/uploads/server/unban.lua
@@ -1,40 +1,65 @@
 local BanTable = iin.BannedUsers
 
-function Unban( cid )
-	assert( isstring( cid ), "bad argument #1 to 'Unban' (string expected, got " .. type( cid ) .. ")" )
 
-	if string.match( cid, "^STEAM_%d:%d:%d+$" ) then
-		cid = util.SteamIDTo64( cid )
+function Unban(cid)
+	assert(isstring(cid), "bad argument #1 to 'Unban' (string expected, got " .. type(cid) .. ")")
+
+	if string.match(cid, "^STEAM_%d:%d:%d+$") then
+		cid = util.SteamIDTo64(cid)
 	end
 	
-	if not string.match( cid, "^%d+$" ) then
-		error( "Incorrect cid to unban" )
+	if not string.match(cid, "^%d+$") then
+		error("Incorrect cid to unban")
 	end
 	
-	local BanFile = file.Read( "iin/bans.txt" )
-	local BanFile_table = string.Explode( "\n", BanFile )
+	BanFile = file.Read("iin/bans.txt")
+	local BanFile_table = string.Explode("\n", BanFile)
 	
-	for i, line in pairs( BanFile_table ) do
-		local Player64ID = string.match( line, "^%d+" )
+	for i, line in ipairs(BanFile_table) do
+		local Player64ID = string.match(line, "^%d+")
 		
 		if Player64ID == cid then
-			table.remove( BanFile_table, i )
-			for j, ply in pairs( BanTable ) do
+			table.remove(BanFile_table, i)
+			for j, ply in ipairs(BanTable) do
 				if ply.cid == cid then
-					table.remove( BanTable, j )
+					table.remove(BanTable, j)
 					break
 				end
 			end
 			
-			local BanFile = table.concat( BanFile_table, "\n" )
+			local BanFile = table.concat(BanFile_table, "\n")
 			
-			file.Write( "iin/bans.txt", BanFile )
+			file.Write("iin/bans.txt", BanFile)
 			
-			print( cid .. " has been unbanned" )
+			print(cid .. " has been unbanned")
 			
 			return
 		end
 	end
 	
-	print( "Player with cid " .. cid .. " not found in ban list" )
+	print("Player with cid " .. cid .. " not found in ban list")
+end
+
+
+function BanReason(cid)
+	assert(isstring(cid), "bad argument #1 to 'Unban' (string expected, got " .. type(cid) .. ")")
+
+	if string.match(cid, "^STEAM_%d:%d:%d+$") then
+		cid = util.SteamIDTo64(cid)
+	end
+	
+	if not string.match(cid, "^%d+$") then
+		error("Incorrect cid to unban")
+	end
+	
+	BanFile = file.Read("iin/bans.txt")
+	local BanFile_table = string.Explode("\n", BanFile)
+	
+	for i, line in ipairs(BanFile_table) do
+		local Player64ID, reason = string.match(line, "^(%d+)%s(.*)")
+		
+		if Player64ID == cid then
+			return reason
+		end
+	end
 end
