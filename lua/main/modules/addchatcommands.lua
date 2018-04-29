7f50414b9ae11db6e16aa37c98bddbee1cf4445a
CornerPin <cornerpinart@gmail.com>
2018-04-29 22:41:57 +0300

    It is hexademical ok

--- lua/main/modules/addchatcommands.lua
+++ lua/main/modules/addchatcommands.lua
@@ -502,7 +502,7 @@ iin.AddCommand("w",function(ply,line)
 	--if ply~= me then return end
 	print(urlencode(line))
 	http.Fetch("http://api.wolframalpha.com/v2/query?input="..urlencode(line).."&appid=K677A9-RYWJPVUUJK",function(s)
-		s = string.gsub(s, [[\:(%d%d%d%d)]], function(code)
+		s = string.gsub(s, [[\:([%da-f][%da-f][%da-f][%da-f])]], function(code)
 			return utf8.char(tonumber(code, 16))
 		end)
 		
