20137740a431d3c67bd9f992e4e06e8985775e68
CornerPin <cornerpinart@gmail.com>
2018-06-10 13:04:21 +0300

    Improved weapon_sound code

--- lua/uploads/client/weapon_sounds.lua
+++ lua/uploads/client/weapon_sounds.lua
@@ -1,45 +1,43 @@
 local weapon_sounds = CreateClientConVar("weapon_sounds", "0")
 
 local weaponsounds = {
-	['ar2'] = {
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/bum1.mp3',
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/bum3.mp3'
-	},
-	['shotgun'] = {
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/zvuki_seksa.mp3'
-	},
-	['crossbow'] = {
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/nya.mp3'
+	["ar2"] = {
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/bum1.mp3",
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/bum3.mp3"
 	},
 
-	['iceaxe'] = {
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/kus.mp3'
+	["shotgun"] = {
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/zvuki_seksa.mp3"
 	},
 
-	['rpg'] = {
-		'https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/ooop.mp3'
+	["crossbow"] = {
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/nya.mp3"
+	},
+
+	["iceaxe"] = {
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/kus.mp3"
+	},
+
+	["rpg"] = {
+		"https://raw.githubusercontent.com/OldOverusedMeme/cancer/master/ooop.mp3"
 	}
 }
 
-weaponsounds['pistol'] = weaponsounds["ar2"]
+weaponsounds["pistol"] = weaponsounds["ar2"]
 
-local function zvuki_nah(tabl)
+local function weaponSounds(data)
 	if not weapon_sounds:GetBool() then return end
-	local Ent = tabl.Entity
-	local SName = tabl.SoundName
-	local wepname = string.Explode('/',SName)[2]
+
+	local ent = data.Entity
+	local soundname = data.SoundName
+	local wepname = string.match(soundname, "^%)weapons/(%w+)")
 	
-	if Ent and Ent:IsPlayer() and Ent:IsAdmin() then
+	if weaponsounds[wepname] and ent and ent:IsPlayer() and ent:IsAdmin() then
+		local rand = table.Random(weaponsounds[wepname])
+		ent:PlayURL(rand)
 		
-		if string.find( SName, 'weapons/' ) and weaponsounds[ wepname ] then
-			local rand = table.Random(weaponsounds[ wepname ])
-			
-			Ent:PlayURL( rand )
-			
-			return false
-		end
-		
+		return false
 	end
 end
 
-hook.Add( 'EntityEmitSound', 'tak_pososi_se4asje', zvuki_nah )
+hook.Add("EntityEmitSound", "weaponSounds", weaponSounds)
