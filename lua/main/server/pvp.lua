5550c4de2bd718c8610d8da2789c20d89a57c3ed
CornerPin <cornerpinart@gmail.com>
2018-06-10 12:25:52 +0300

    Revert !build command and antispam, better type checks in PLAYER.SetPvP

--- lua/main/server/pvp.lua
+++ lua/main/server/pvp.lua
@@ -2,33 +2,33 @@ local tag = "pvp"
 local changeModeDelay = 5
 
 local pvpWeapons = {
-   weapon_357 = true,
-   weapon_ar2 = true,
-   weapon_bugbait = true,
-   weapon_crossbow = true,
-   weapon_crowbar = true,
-   weapon_frag = true,
-   weapon_smg1 = true,
-   weapon_slam = true,
-   weapon_shotgun = true,
-   weapon_rpg = true,
-   weapon_pistol = true,
-   weapon_physcannon = true
+	"weapon_357",
+	"weapon_ar2",
+	"weapon_bugbait",
+	"weapon_crossbow",
+	"weapon_crowbar",
+	"weapon_frag",
+	"weapon_smg1",
+	"weapon_slam",
+	"weapon_shotgun",
+	"weapon_rpg",
+	"weapon_pistol",
+	"weapon_physcannon"
 }
 
 local pvpAmmo = {
-   "AR2",
-   "AR2AltFire",
-   "Pistol",
-   "SMG1",
-   "357",
-   "XBowBolt",
-   "Buckshot",
-   "RPG_Round",
-   "SMG1_Grenade",
-   "Grenade",
-   "slam",
-   "AlyxGun"
+	"AR2",
+	"AR2AltFire",
+	"Pistol",
+	"SMG1",
+	"357",
+	"XBowBolt",
+	"Buckshot",
+	"RPG_Round",
+	"SMG1_Grenade",
+	"Grenade",
+	"slam",
+	"AlyxGun"
 }
 
 timer.Simple(0, function()
@@ -81,7 +81,8 @@ end
 
 local PLAYER = FindMetaTable("Player")
 function PLAYER:SetPvP(state, silent)
-	assert(IsValid(self) and self:IsPlayer(), "Should be used on a player")
+	assert(type(self) == "Player", "bad argument #1 to 'SetPvP' (Player expected, got " .. type(self) .. ")")
+	assert(IsValid(self), "bad argument #1 to 'SetPvP' (Tried to use a NULL entity!)")
 	assert(isbool(state), "bad argument #2 to 'SetPvP' (boolean expected, got " .. type(state) .. ")")
 	
 	self.pvp = state
@@ -110,22 +111,77 @@ end
 
 
 iin.AddCommand("pvp", function(ply)
-           ply.pvp = not ply.pvp
-           ply:SetPvP(ply.pvp)
-           ply:Spawn()
-           iin.Msg(
-              nil,
-             Color(255, 187, 0),
-              " ● ",
-              ply,
-              color_white,
-              " перешёл в ",
-              ply.pvp and Color(255, 0, 0) or Color(0, 255, 0),
-              ply.pvp and "PvP" or "строительный",
-              color_white,
-              " режим."
-           )
+	if ply.pvp then
+		ply:ChatPrint(
+			"Вы уже находитесь в PvP режиме.\n" ..
+			"Для перехода в строительный режим используйте !build"
+		)
+	elseif CurTime() >= ply.pvpNextChangeMode then
+		ply.pvpNextChangeMode = CurTime() + changeModeDelay
+		
+		ply:SetPvP(true)
+		ply:Spawn()
+		
+		iin.Msg(
+			nil,
+			Color(255, 187, 0),
+			" ● ",
+			ply,
+			color_white,
+			" перешёл в ",
+			Color(255, 0, 0),
+			"PvP",
+			color_white,
+			" режим."
+		)
+	else
+		local seconds = math.ceil(ply.pvpNextChangeMode - CurTime())
+
+		ply:ChatPrint(string.format(
+			"Подождите %d %s, чтобы сменить режим",
+			seconds,
+			declension(seconds, "секунду", "секунды", "секунд")
+		))
+	end
 end, "players", true)
+
+
+iin.AddCommand("build", function(ply)
+	if not ply.pvp then
+		ply:ChatPrint(
+			"Вы уже находитесь в строительном режиме.\n" ..
+			"Для перехода в PvP режим используйте !pvp"
+		)
+	elseif CurTime() >= ply.pvpNextChangeMode then
+		ply.pvpNextChangeMode = CurTime() + changeModeDelay
+
+		ply:SetPvP(false)
+		ply:StripWeapons()
+		ply:Spawn()
+		
+		iin.Msg(
+			nil,
+			Color(255, 187, 0),
+			" ● ",
+			ply,
+			color_white,
+			" перешёл в ",
+			Color(0, 255, 0),
+			"строительный",
+			color_white,
+			" режим."
+		)
+	else
+		local seconds = math.ceil(ply.pvpNextChangeMode - CurTime())
+
+		ply:ChatPrint(string.format(
+			"Подождите %d %s, чтобы сменить режим",
+			seconds,
+			declension(seconds, "секунду", "секунды", "секунд")
+		))
+	end
+end, "players", true)
+
 
 hook.Add("PlayerInitialSpawn", tag, function(ply)
 	ply.pvpNextChangeMode = 0
@@ -137,16 +193,18 @@ hook.Add("PlayerSpawn", tag, function(ply)
 	if not ply.pvp then
 		ply:GodEnable()
 	else
-           timer.Simple(0.1, function()
-                           if not IsValid(ply) then return end
-                           for k,v in pairs(pvpWeapons) do
-                              ply:Give(k)
-                           end
-                           for k,v in pairs(pvpAmmo) do
-                              ply:GiveAmmo(9999, v, true)
-                           end
-           end)
-        end
+		timer.Simple(0, function()
+			if not IsValid(ply) then return end
+
+			for i, wepname in ipairs(pvpWeapons) do
+				ply:Give(wepname)
+			end
+
+			for i, ammoname in ipairs(pvpAmmo) do
+				ply:GiveAmmo(9999, ammoname, true)
+			end
+		end)
+	end
 end)
 
 
